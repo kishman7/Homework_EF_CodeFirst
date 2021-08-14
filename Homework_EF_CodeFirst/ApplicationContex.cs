215c675420a7626f0a352977250ab61d29eade5a
kishman7 <kishman_us@ukr.net>
2021-08-14 21:04:55 +0300

    Add Homework_EF_CodeFirst

---
+++ Homework_EF_CodeFirst/ApplicationContex.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Homework_EF_CodeFirst
+{
+    public class ApplicationContex : DbContext
+    {
+        // Контекст настроен для использования строки подключения "ApplicationContex" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "Homework_EF_CodeFirst.ApplicationContex" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "ApplicationContex" 
+        // в файле конфигурации приложения.
+        public ApplicationContex()
+            : base("name=ApplicationContex")
+        {
+        }
+
+        public virtual DbSet<Game> Games { get; set; }
+
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
---
+++ Homework_EF_CodeFirst/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_EF_CodeFirst
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string NameGame { get; set; }
+        public string ProducerGame { get; set; }
+        public string StyleGame { get; set; }
+        public DateTime DateGame { get; set; }
+
+        // Навігаційні властивості можуть бути, якщо будуть зовнішні ключі
+    }
+}
